000412f7446f9d5608d35c82a021af920b045532
Jason <wangsanmu5@163.com>
2025-04-28 10:50:53 +0800

    feat(update-server): add poetry seed

--- apps/server/prisma/seed.ts
+++ apps/server/prisma/seed.ts
@@ -1,9 +1,126 @@
 import { PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const prisma = new PrismaClient();
 
+const POETRY_TYPEs = {
+  chu_ci: 'chu_ci',
+  lun_yu: 'lun_yu',
+  shi_jing: 'shi_jing',
+  song_ci: 'song_ci',
+  tang_shi: 'tang_shi',
+  yuan_qu: 'yuan_qu',
+};
+
+const POETRY_TYPE_MAP = {
+  'chu_ci': '楚辞',
+  'lun_yu': '论语',
+  'shi_jing': '诗经',
+  'song_ci': '宋词',
+  'tang_shi': '唐诗',
+  'yuan_qu': '元曲',
+};
+
+async function poetrySeed() {
+  console.log('Start seed poetry');
+  const poetryDir = path.resolve(__dirname, '../data/chinese-poetry');
+  const files = fs.readdirSync(poetryDir).filter(f => f.endsWith('.json'));
+  let total = 0, skipped = 0, inserted = 0;
+
+  const getContent = (type: string, poetry: any) => {
+    if (type === POETRY_TYPEs.chu_ci) {
+      return poetry.content;
+    } else if (type === POETRY_TYPEs.lun_yu) {
+      return poetry.paragraphs;
+    } else if (type === POETRY_TYPEs.shi_jing) {
+      return poetry.content;
+    } else if (type === POETRY_TYPEs.song_ci) {
+      return poetry.paragraphs;
+    } else if (type === POETRY_TYPEs.tang_shi) {
+      return poetry.paragraphs;
+    } else if (type === POETRY_TYPEs.yuan_qu) {
+      return poetry.paragraphs;
+    }
+    return poetry.content;
+  }
+
+  const getTags = (type: string, poetry: any) => {
+    if (type === POETRY_TYPEs.chu_ci) {
+      return [poetry.section];
+    } else if (type === POETRY_TYPEs.lun_yu) {
+      return [poetry.chapter];
+    } else if (type === POETRY_TYPEs.shi_jing) {
+      return [poetry.chapter, poetry.section];
+    } else if (type === POETRY_TYPEs.song_ci) {
+      return poetry.tags || [];
+    } else if (type === POETRY_TYPEs.tang_shi) {
+      return poetry.tags || [];
+    } else if (type === POETRY_TYPEs.yuan_qu) {
+      return [poetry.dynasty];
+    }
+    return poetry.tags || [];
+  }
+
+  const getAuthor = (type: string, poetry: any) => {
+    if (type === POETRY_TYPEs.chu_ci) {
+      return poetry.author;
+    } else if (type === POETRY_TYPEs.lun_yu) {
+      return '孔子';
+    } else if (type === POETRY_TYPEs.shi_jing) {
+      return '';
+    } else if (type === POETRY_TYPEs.song_ci) {
+      return poetry.author;
+    } else if (type === POETRY_TYPEs.tang_shi) {
+      return poetry.author;
+    } else if (type === POETRY_TYPEs.yuan_qu) {
+      return poetry.author;
+    }
+    return poetry.author;
+  }
+
+  for (const file of files) {
+    const filePath = path.join(poetryDir, file);
+    const json = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    if (!Array.isArray(json)) continue;
+    const source = 1; // 1: 诗词库, 2: 用户
+    const fileType = path.basename(file, '.json');
+    console.log('Start seed poetry: ', fileType);
+    for (const item of json) {
+      // convert to poetry
+      const poetry = {
+        title: fileType === POETRY_TYPEs.lun_yu ? item.chapter : item.title,
+        content: getContent(fileType, item),
+        author: getAuthor(fileType, item),
+        type: POETRY_TYPE_MAP[fileType],
+        tags: getTags(fileType, item),
+        source,
+      };
+      // insert or skip
+      const exists = await prisma.poetry.findFirst({
+        where: {
+          title: poetry.title,
+          author: poetry.author,
+          type: poetry.type,
+        },
+      });
+      if (exists) {
+        skipped++;
+        continue;
+      }
+      await prisma.poetry.create({ data: poetry });
+      inserted++;
+      total++;
+    }
+  }
+  console.log(`Poetry successfully seeded. Inserted: ${inserted}, Skipped: ${skipped}`);
+}
+
 async function main() {
+  console.log('Start seed');
+  
+  console.log('Start seed role')
   const adminRole = await prisma.role.upsert({
     where: { name: 'admin' },
     update: {},
@@ -16,7 +133,7 @@ async function main() {
     create: { name: 'user' },
   });
 
-  // Permissions
+  console.log('Start seed permission')
   const createUserPermission = await prisma.permission.upsert({
     where: { name: 'createUser' },
     update: {},
@@ -47,7 +164,7 @@ async function main() {
     create: { name: 'viewUser' },
   });
 
-  // Assign permissions to roles
+  console.log('Start seed role permission')
   await prisma.rolePermission.createMany({
     data: [
       { roleId: adminRole.id, permissionId: createUserPermission.id },
@@ -67,6 +184,7 @@ async function main() {
     skipDuplicates: true,
   });
 
+  console.log('Start seed root user')
   const rootUser = await prisma.user.upsert({
     where: { email: 'root@example.com' },
     update: {},
@@ -87,7 +205,11 @@ async function main() {
     },
   });
 
-  console.log('Permissions successfully assigned to roles.');
+  console.log('Roles and permissions successfully seeded.');
+
+  await poetrySeed();
+
+  console.log('All successfully seeded.');
 }
 
 main()
