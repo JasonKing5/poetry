1707244a73ac098e4b3964dc675eae39bfd4dc75
Jason <wangsanmu5@163.com>
2025-04-28 09:44:39 +0800

    feat(update-server): add run seed command

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,14 +1,9 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RolesPermissionsGuard } from './common/guards/roles-permissions.guard';
-import { execSync } from 'child_process';
-import { PrismaClient } from '@prisma/client';
 import { JwtService } from '@nestjs/jwt';
 
 async function bootstrap() {
-  const prisma = new PrismaClient();
-  execSync('ts-node ./prisma/seed.ts');
-
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
