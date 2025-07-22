2d8459eba41a3c7d74b5022b23b7958c5b1adf9a
Jason <wangsanmu5@163.com>
2025-07-22 09:01:52 +0800

    feat(server): embedding init script

--- apps/server/scripts/db-init/embadding.ts
+++ apps/server/scripts/db-init/embadding.ts
@@ -17,9 +17,9 @@ async function getPoemsCountForEmbedding() {
   return await prisma.poem.count({
     where: {
       isDeleted: false,
-      id: {
-        lt: 101
-      }
+      // id: {
+      //   lt: 101
+      // }
     },
   });
 }
